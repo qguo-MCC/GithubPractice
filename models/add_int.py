34624185460e560bdfeae466720679c7cee78f32
qguo-MCC <qguo@mcc.ca>
2022-09-23 14:55:08 -0400

    Change an integer value

--- models/add_int.py
+++ models/add_int.py
@@ -26,9 +26,11 @@ if __name__ == '__main__':
     from datetime import datetime
     save_dir = Path('logs')
     logger, warnings_logger = logger_creator(save_dir.joinpath(f'add_two_int_main_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log'))
-    x = 1
+
+    x = 2
     y = 2
     result = add_int(x=x, y=y)
+
     #use debug to find out how does the function work step by step or if there was an error.
     logger.info(f'The result of adding {x} and {y} is: {result}')
     #use pythong logging instead of printing to save important messages to files.
